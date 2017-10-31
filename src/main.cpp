5c2880874a5177267cb1c80e2fd68c96c98d9975
Semyon <snepomny@axmor.com>
2017-10-31 09:30:26 +0700

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,37 @@
+#include <Arduino.h>
+
+#include <Arduino.h>
+
+int IN_PIN = 34;
+int OUT_PIN = 2;
+
+int currentState = true;
+
+int arrayValue[100];
+int iterator=0;
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println();
+  Serial.print("Start...");
+
+  pinMode(OUT_PIN, OUTPUT);
+  pinMode(IN_PIN, INPUT);
+}
+
+void loop() {
+
+int val = analogRead(IN_PIN);
+
+if (iterator>=99) {
+  iterator = 0;
+  //Serial.println(arrayValue[0]);
+}
+
+arrayValue[iterator] = val;
+
+iterator++;
+
+currentState= !currentState;
+digitalWrite(OUT_PIN, currentState);
+}
