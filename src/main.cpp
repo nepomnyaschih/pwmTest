63b53d8987ce8920bc9bdcaae7730c80f7128eee
Semyon <snepomny@axmor.com>
2017-11-01 19:05:08 +0700

    added threads

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,38 @@
 int IN_PIN = 34;
 int OUT_PIN = 2;
 
+int OUT_SIN_PIN = 25; //is this without pwm modulation? https://esp-idf.readthedocs.io/en/v2.0/api/peripherals/dac.html
+
 int currentState = true;
 
 int arrayValue[100];
 int iterator=0;
+
+void firstCoreTask( void * pvParameters ){
+    while(true){
+
+      int val = analogRead(IN_PIN);
+
+      if (iterator>=99) {
+        iterator = 0;
+        Serial.println(arrayValue[0]);
+      }
+
+      arrayValue[iterator] = val;
+
+      iterator++;
+
+      currentState= !currentState;
+      digitalWrite(OUT_PIN, currentState);
+    }
+}
+
+void secondCoreTask( void * pvParameters ){
+    while(true){
+        dacWrite(OUT_SIN_PIN, random(0, 255));
+    }
+}
+
 
 void setup() {
   Serial.begin(115200);
@@ -14,22 +42,30 @@ void setup() {
   Serial.print("Start...");
 
   pinMode(OUT_PIN, OUTPUT);
+  pinMode(OUT_SIN_PIN, OUTPUT);
   pinMode(IN_PIN, INPUT);
+
+
+  xTaskCreatePinnedToCore(
+                    firstCoreTask,   /* Function to implement the task */
+                    "firstCoreTask", /* Name of the task */
+                    10000,      /* Stack size in words */
+                    NULL,       /* Task input parameter */
+                    0,          /* Priority of the task */
+                    NULL,       /* Task handle. */
+                    0);  /* Core where the task should run */
+
+  xTaskCreatePinnedToCore(
+                    secondCoreTask,   /* Function to implement the task */
+                    "secondCoreTask", /* Name of the task */
+                    10000,      /* Stack size in words */
+                    NULL,       /* Task input parameter */
+                    0,          /* Priority of the task */
+                    NULL,       /* Task handle. */
+                    1);  /* Core where the task should run */
+
+
 }
 
 void loop() {
-
-int val = analogRead(IN_PIN);
-
-if (iterator>=99) {
-  iterator = 0;
-  Serial.println(arrayValue[0]);
-}
-
-arrayValue[iterator] = val;
-
-iterator++;
-
-currentState= !currentState;
-digitalWrite(OUT_PIN, currentState);
 }
